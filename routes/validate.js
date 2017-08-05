55a83709bf9956202a360dc35b73d845a080b36e
pampas93 <abhijit.93@hotmail.com>
2017-08-05 13:14:07 -0400

    Navigation bar + Validate json done

--- routes/validate.js
+++ routes/validate.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('validate', {status: '', json : '' });
 });
 
+router.post('/', function(req, res, next){
+  var jsonString = req.body.jsontextarea;
+
+  var validity = isJson(jsonString);
+  try{
+    jsonString = JSON.stringify(eval("(" + jsonString + ")"), null, "\t");
+  }catch(e){
+      
+  }
+  
+  res.render('validate', { status: validity, json: jsonString });
+});
+
+function isJson(data) {
+	try {
+		JSON.parse(data);
+	} catch (e) {
+		//alert("Not a Valid JSON String");
+		return "Sorry, not Valid!";
+	}
+    //alert("Valid Json")
+    return "Valid!";
+}
+
+
 module.exports = router;
